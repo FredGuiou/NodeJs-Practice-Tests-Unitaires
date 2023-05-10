37a58adb251c343e610482cd949197617639a7bf
FredGuiou <99122562+FredGuiou@users.noreply.github.com>
2023-05-10 12:54:25 +0200

    Done ✅ : 03.js test

--- 03.js
+++ 03.js
@@ -1,14 +1,16 @@
-const User = {
-    name: "fraxken",
-    callGreet: 0,
-    addProperty() {
-        return Reflect.set(this, "hello", "world!");
-    },
-    greet() {
-        this.callGreet = this.callGreet > 2 ?  0 : this.callGreet + 1;
+"use strict";
 
-        return `hello ${this.name}`;
-    }
-}
+const User = {
+  name: "fraxken",
+  callGreet: 0,
+  addProperty() {
+    return Reflect.set(this, "hello", "world!");
+  },
+  greet() {
+    this.callGreet = this.callGreet > 2 ? 0 : this.callGreet + 1;
+
+    return `hello ${this.name}`;
+  }
+};
 
 module.exports = Object.seal(User);
---
+++ test/03.test.js
@@ -0,0 +1,46 @@
+"use strict";
+// Import des nodeJs dependencies
+const { describe, it } = require("node:test");
+const assert = require("node:assert");
+
+// Import des fonctions à tester
+// const { name, callGreet, addProperty, greet } = require("../03.js");
+
+const User = require("../03.js");
+
+describe("User name test", () => {
+  it("Should have the correct value for name property", () => {
+    assert.strictEqual(User.name, "fraxken");
+  });
+});
+
+describe("User callGreet test", () => {
+  it("Should have the correct initial value for callGreet property", () => {
+    assert.strictEqual(User.callGreet, 0);
+  });
+});
+
+describe("User Object.seal statement", () => {
+  it("Should be sealed", () => {
+    assert.ok(Object.isSealed(User), "should equal true because the provided Object is expected to be sealed");
+  });
+});
+
+describe("User greet function", () => {
+  it("Should return a correct greeting sentence", () => {
+    const greeting = User.greet();
+    assert.deepStrictEqual(greeting, "hello fraxken");
+  });
+
+  it("Should increment callGreet property +1 untill >2 and reset to 0", () => {
+    User.callGreet = 0;
+    User.greet();
+    assert.equal(User.callGreet, 1);
+    User.greet();
+    assert.equal(User.callGreet, 2);
+    User.greet();
+    assert.equal(User.callGreet, 3);
+    User.greet();
+    assert.equal(User.callGreet, 0);
+  });
+});
