891aca89299a2ba219d931d6caf2b52bc7438206
FredGuiou <99122562+FredGuiou@users.noreply.github.com>
2023-05-21 11:27:15 +0200

    Adding useStrict on 06.js

--- 06.js
+++ 06.js
@@ -1,23 +1,26 @@
+"use strict";
+
 const oP = {
-    foo: 10
+  foo: 10
 };
 
 const handler = {
-    get(obj, prop) {
-        if (!Reflect.has(obj, prop)) {
-            throw new Error(`Unknown property ${prop} in target object!`);
-        }
-
-        return Reflect.get(obj, prop) ** 2;
-    },
-    set(obj, prop, value) {
-        if (typeof value !== "number") {
-            throw new TypeError("value must be a number");
-        }
-
-        Reflect.set(obj, prop, value);
-        return true;
+  get(obj, prop) {
+    if (!Reflect.has(obj, prop)) {
+      throw new Error(`Unknown property ${prop} in target object!`);
     }
-}
+
+    return Reflect.get(obj, prop) ** 2;
+  },
+  set(obj, prop, value) {
+    if (typeof value !== "number") {
+      throw new TypeError("value must be a number");
+    }
+
+    Reflect.set(obj, prop, value);
+
+    return true;
+  }
+};
 
 module.exports = new Proxy(oP, handler);
