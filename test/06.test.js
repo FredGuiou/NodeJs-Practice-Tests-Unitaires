5e3398b95e2a362a11ce1ac67a4060fb027862c1
FredGuiou <99122562+FredGuiou@users.noreply.github.com>
2023-05-22 11:09:20 +0200

    Test on 06.js

---
+++ test/06.test.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// Import des nodeJs dependencies
+const { describe, it } = require("node:test");
+const assert = require("node:assert");
+
+// Import des fonctions à tester
+// const { oP, handler } = require("./06.js");
+const proxy = require("../06.js");
+
+// Ensemble des test
+
+describe("handler_get", () => {
+  it("Should throw a new Error if property \"prop\" is unknown", () => {
+    assert.throws(() => {
+      const unknownProp = proxy.unknownProp;
+
+      return unknownProp;
+    }, Error);
+  });
+
+  it("Should return square value of object property", () => {
+    assert.strictEqual(proxy.foo, 100);
+  });
+});
+
+describe("handler_set", () => {
+  it("Should throw a new TypeError if typeof value isn't \"number\"", () => {
+    assert.throws(() => {
+      proxy.foo = "20";
+    }, TypeError);
+  });
+
+  it("Should set the value on the object's property", () => {
+    assert.strictEqual(proxy.foo = 30, 30);
+  });
+});
+
+// Documentation :
+// https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Reflect/has
+// https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Reflect/set
